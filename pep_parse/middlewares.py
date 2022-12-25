e3af1e7a011e0385ad6ea19ee478494529c95e2e
Кирилл Яснов <kirillasnov@MacBook-Air-Kirill.local>
2022-12-25 11:17:46 +0500

    first review

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -1,8 +1,5 @@
 from scrapy import signals
 
-# useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
-
 
 class PepParseSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
